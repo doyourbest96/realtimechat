921fb1b3291290d81690f84e1c0c47b56064f100
doyourbest <creative555.dev@gmail.com>
2024-12-26 02:37:51 -0600

    [update] server list

--- src/components/ServerList.tsx
+++ src/components/ServerList.tsx
@@ -1,6 +1,12 @@
 import { UserIcon } from './UserIcon'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 
+interface UserIconProps {
+  src: string;
+  alt: string;
+  size?: 'sm' | 'md' | 'lg';
+}
+
 interface Server {
   id: string
   name: string
@@ -15,16 +21,16 @@ interface ServerListProps {
 
 export function ServerList({ servers, activeServerId, onServerClick }: ServerListProps) {
   return (
-    <div className="w-20 bg-gray-900 p-3 space-y-4 overflow-y-auto">
+    <div className="w-16 md:w-20 bg-gray-900 p-2 md:p-3 space-y-2 md:space-y-4 overflow-y-auto flex flex-col items-center">
       <TooltipProvider>
         {servers.map((server) => (
           <Tooltip key={server.id}>
             <TooltipTrigger asChild>
               <button
                 onClick={() => onServerClick(server.id)}
-                className={`w-full flex justify-center items-center h-12 ${activeServerId === server.id ? 'bg-gray-700 rounded-xl' : ''}`}
+                className={`w-10 h-10 md:w-12 md:h-12 flex justify-center items-center ${activeServerId === server.id ? 'bg-gray-700 rounded-xl' : ''}`}
               >
-                <UserIcon src={server.icon} alt={server.name} />
+                <UserIcon src={server.icon} alt={server.name} size="sm" />
               </button>
             </TooltipTrigger>
             <TooltipContent side="right">
